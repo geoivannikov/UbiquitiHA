d5c913ebc893147b52d095a23a4bc772360eace0
Ivannikov Georgiy <georgiy.ivannikov@seznam.cz>
2025-10-02 17:12:08 +0200

    xc proj inited

---
+++ UbiquitiHA/UbiquitiHAApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbiquitiHAApp.swift
+//  UbiquitiHA
+//
+//  Created by Ivannikov-EXTERNAL Georgiy on 02.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct UbiquitiHAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UbiquitiHATests/UbiquitiHATests.swift
@@ -0,0 +1,17 @@
+//
+//  UbiquitiHATests.swift
+//  UbiquitiHATests
+//
+//  Created by Ivannikov-EXTERNAL Georgiy on 02.10.2025.
+//
+
+import Testing
+@testable import UbiquitiHA
+
+struct UbiquitiHATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
